102cd99fc9cd6b07f35036153d1c0307c6744f29
Viliam Holly <viliamholly@Viliams-MacBook-Air.local>
2024-04-08 11:18:43 +0200

    Improve naming, add tick counter and other features

--- src/main.cc
+++ src/main.cc
@@ -4,12 +4,15 @@
 #include <iostream>
 #include <utility>
 
-constexpr int cell_size = 10;
-constexpr int row_count = 50;
-constexpr int collum_count = 100;
-constexpr Color cell_color = Color{64, 224, 208, 255};
+constexpr int kCellSize = 10;
+constexpr int kRowCount = 50;
+constexpr int kColCount = 100;
+constexpr Color kCellCol = Color{64, 224, 208, 255};
+
 bool game_paused = true;
-std::array<std::array<bool, collum_count>, row_count> cells;
+std::array<std::array<bool, kColCount>, kRowCount> cells;
+float curr_time = 0;
+float tick_delay = 0.5;
 
 int CountNeighbours(int row_idx, int col_idx) {
   static std::array<std::pair<int, int>, 8> neighbour_offs = {
@@ -22,9 +25,9 @@ int CountNeighbours(int row_idx, int col_idx) {
   for (const auto &[row_off, col_off] : neighbour_offs) {
     int new_row_idx = row_idx + row_off;
     int new_col_idx = col_idx + col_off;
-    if (new_row_idx < 0 || new_row_idx >= cells.size())
+    if (new_row_idx < 0 || new_row_idx >= kRowCount)
       continue;
-    if (new_col_idx < 0 || new_col_idx >= cells.at(0).size())
+    if (new_col_idx < 0 || new_col_idx >= kColCount)
       continue;
 
     if (cells.at(new_row_idx).at(new_col_idx))
@@ -43,20 +46,20 @@ void RenderFrame() {
     int cell_idx = 0;
     for (const auto &cell : row) {
       if (cell)
-        DrawRectangle(cell_idx * cell_size, row_idx * cell_size, cell_size,
-                      cell_size, cell_color);
+        DrawRectangle(cell_idx * kCellSize, row_idx * kCellSize, kCellSize,
+                      kCellSize, kCellCol);
       cell_idx++;
     }
     row_idx++;
   }
 
-  for (int curr_row_idx = 0; curr_row_idx < row_count; curr_row_idx++) {
-    for (int curr_col_idx = 0; curr_col_idx < collum_count; curr_col_idx++) {
-      DrawLine(curr_col_idx * cell_size, curr_row_idx * cell_size,
-               curr_col_idx * cell_size, row_count * cell_size, WHITE);
+  for (int curr_row_idx = 0; curr_row_idx < kRowCount; curr_row_idx++) {
+    for (int curr_col_idx = 0; curr_col_idx < kColCount; curr_col_idx++) {
+      DrawLine(curr_col_idx * kCellSize, curr_row_idx * kCellSize,
+               curr_col_idx * kCellSize, kRowCount * kCellSize, WHITE);
     }
-    DrawLine(0, curr_row_idx * cell_size, collum_count * cell_size,
-             curr_row_idx * cell_size, WHITE);
+    DrawLine(0, curr_row_idx * kCellSize, kColCount * kCellSize,
+             curr_row_idx * kCellSize, WHITE);
   }
   EndDrawing();
 }
@@ -66,24 +69,48 @@ void Update() {
     game_paused = !game_paused;
   }
 
-  if (game_paused) {
-    if (!IsMouseButtonPressed(MOUSE_BUTTON_LEFT) &&
-        !IsMouseButtonPressed(MOUSE_BUTTON_RIGHT))
+  else if (IsKeyPressed(KEY_C)) {
+    for (auto &row : cells) {
+      row.fill(false);
+    }
+    game_paused = true;
+  }
+
+  else if (IsKeyPressed(KEY_RIGHT)) {
+    tick_delay /= 2;
+  }
+
+  else if (IsKeyPressed(KEY_LEFT)) {
+    tick_delay *= 2;
+  }
+
+  else if (game_paused) {
+    if (!IsMouseButtonDown(MOUSE_BUTTON_LEFT) &&
+        !IsMouseButtonDown(MOUSE_BUTTON_RIGHT))
       return;
 
-    int row_idx = floor(GetMouseY() / cell_size);
-    int col_idx = floor(GetMouseX() / cell_size);
+    int row_idx = floor(GetMouseY() / kCellSize);
+    int col_idx = floor(GetMouseX() / kCellSize);
+
+    if (row_idx >= kRowCount || col_idx >= kColCount)
+      return;
 
     cells.at(row_idx).at(col_idx) =
-        IsMouseButtonPressed(MOUSE_BUTTON_LEFT) ? true : false;
+        IsMouseButtonDown(MOUSE_BUTTON_LEFT) ? true : false;
 
     return;
   }
 
+  if (curr_time < tick_delay) {
+    curr_time += GetFrameTime();
+    return;
+  }
+
+  curr_time -= tick_delay;
   auto updated_cells = cells;
 
-  for (int curr_row_idx = 0; curr_row_idx < row_count; curr_row_idx++) {
-    for (int curr_col_idx = 0; curr_col_idx < collum_count; curr_col_idx++) {
+  for (int curr_row_idx = 0; curr_row_idx < kRowCount; curr_row_idx++) {
+    for (int curr_col_idx = 0; curr_col_idx < kColCount; curr_col_idx++) {
       int n_count = CountNeighbours(curr_row_idx, curr_col_idx);
       bool is_alive = cells.at(curr_row_idx).at(curr_col_idx);
 
@@ -102,8 +129,8 @@ void Update() {
 }
 
 int main(void) {
-  InitWindow(collum_count * cell_size, row_count * cell_size, "Game of Life");
-  SetTargetFPS(60);
+  InitWindow(kColCount * kCellSize, kRowCount * kCellSize, "Game of Life");
+  SetTargetFPS(GetMonitorRefreshRate(GetCurrentMonitor()));
 
   while (!WindowShouldClose()) {
     Update();
